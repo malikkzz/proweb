148f3d48791c96037f6bcb5a329cc5bb7ed8f5d3
Irpannnz <irfanzaki2412@gmail.com>
2025-03-21 10:48:54 +0800

    Irfan

--- frontend/src/components/ProdukList.jsx
+++ frontend/src/components/ProdukList.jsx
@@ -7,21 +7,26 @@ function ProdukList() {
 
   useEffect(() => {
     axios.get('http://localhost:3001/produk')
-      .then((response) => {
-        setProduk(response.data);
-      })
-      .catch((error) => {
-        console.error('Terjadi error:', error);
-      });
+      .then((response) => setProduk(response.data))
+      .catch((error) => console.error(error));
   }, []);
+
+  const handleDelete = (id) => {
+    axios.delete(`http://localhost:3001/produk/${id}`)
+      .then(() => {
+        setProduk(produk.filter((p) => p.id !== id));
+      })
+      .catch(err => console.error(err));
+  };
 
   return (
     <div>
-      <h2>Daftar Produk (From Database)</h2>
+      <h2>Daftar Produk</h2>
       <ul>
         {produk.map((item) => (
           <li key={item.id}>
             {item.nama} - Rp{item.harga}
+            <button onClick={() => handleDelete(item.id)}>Delete</button>
           </li>
         ))}
       </ul>
